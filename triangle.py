df87ee2ccdb3bc3aa0af12532535d0f83e0f3f37
Tigran Hakobyan <tik.hakobyan@gmail.com>
2014-08-27 10:18:20 -0400

    first commit

---
+++ triangle.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+def triangle():
+	forward(100)
+	left(120)
+	forward(100)
+	left(120)
+	forward(100)
+	left(120)
+
+def main():
+	triangle()
+	left(90)
+	triangle()
+	left(90)
+	triangle()
+	left(90)
+	triangle()
+	left(90)
+	raw_input("Enter to close!")
+
+main()
