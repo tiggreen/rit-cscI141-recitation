0625d7ca5784d632809427e5444d1b5a7b6646ea
Tigran Hakobyan <tik.hakobyan@gmail.com>
2014-10-24 12:52:54 -0400

    Some code is added

--- LinkedList.py
+++ LinkedList.py
@@ -9,6 +9,8 @@ class LinkedList:
 
 #print the linkedlist
 def printList(lst):
+	if lst is None:
+		return None
 	curr = lst.head
 	while curr is not None:
 		print(curr.data)
@@ -55,30 +57,60 @@ def addNode(lst, nodeData):
 
 def count_iter(lst, elem):
 	if lst is None:
-		return None 
-	cnt = 0 
+		return None
 	curr = lst.head
+	count = 0
 	while curr is not None:
 		if curr.data == elem:
-			cnt+=1
+			count += 1 
 		curr = curr.next
-	return cnt
+	return count
 
-def count_rec(head, elem):
+def count_rec(lst,elem):
+	if lst is None:
+		return None
+	return count_rec_util(lst.head, elem)
+
+def count_rec_util(head, elem):
 	if head is None:
 		return 0
 	if head.data == elem:
-		return 1 + count_rec(head.next, elem)
+		return 1 + count_rec_util(head.next, elem)
 	else:
-		return count_rec(head.next, elem)
+		return count_rec_util(head.next, elem)
+
+def remove_rec(lst, elem):
+	if lst is None:
+		return None
+	if lst.head.data == elem:
+		lst.head = lst.head.next
+		return lst
+	else:
+		remove_rec_util(lst.head, elem)
+		return lst
+
+def remove_rec_util(head, elem):
+	if head is None:
+		return None
+	if head.next is None:
+		return None
+	if head.next.data == elem:
+		head.next = head.next.next
+	else:
+		return remove_rec_util(head.next, elem)
 
 def main():
 	ll  = createLinkedList()
 	addNode(ll, 5)
 	addNode(ll, 3)
 	addNode(ll, 5)
+	addNode(ll, 5)
+	addNode(ll, 3)
 	addNode(ll, 4)
-
+	addNode(ll, 5)
+	printList(remove_rec(ll, 3))
+	#printList(ll)
+	
 main()
 	
 
